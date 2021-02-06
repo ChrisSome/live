aa25532eab342224d8edcd30a80d91005d6c1f5c
mac <mac@pc6deMacBook-Pro4.local>
2021-02-06 20:49:50 +0800

    资讯是否点赞

--- App/lib/FrontService.php
+++ App/lib/FrontService.php
@@ -580,7 +580,7 @@ class  FrontService {
             $formatUsers[$v->id] = $v;
         });
         array_walk($operates, function($vi, $ki) use(&$formatOperate){
-            $formatOperate[$vi->user_id] = $vi;
+            $formatOperate[] = $vi->user_id;
         });
         foreach ($informations as $item)
         {
@@ -590,7 +590,7 @@ class  FrontService {
             $data['title'] = $item['title'];
             $data['img'] = $item['img'];
             $data['status'] = $item['status'];
-            $data['is_fabolus'] = $uid ? (isset($formatOperate[$item->id]) ? true : false) : false;
+            $data['is_fabolus'] = $uid ? (in_array($uid, $formatOperate) ? true : false) : false;
             $data['fabolus_number'] = $item['fabolus_number'];
             $data['respon_number'] = $item['respon_number'];
             $data['competition_id'] = $item['competition_id'];
